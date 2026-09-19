79c66ef3ed6a24ec2d370f3a6af640da9b5053ab
haneena-dotcom <kfathimahaneena2005@gmail.com>
2026-09-19 12:07:57 +0530

    updated list program

--- list.c
+++ list.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node
+struct Node
 {
   int data;
   struct Node* next;
 };
 struct Node* top=NULL;
-int is empty()
+int isEmpty()
 {
 	return top == NULL;
 }
 void push(int value)
 {
-struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
 if (newNode ==NULL){
-	Printf("stack overflow!caanot push %d.\n",value);
+printf("stack overflow!caanot push %d.\n",value);
 	return;
 }
 newNode->data = value;
 newNode->next = top;
 top= newNode;
-printf("%d pushed to stack.\n", values);
+printf("%d pushed to stack.\n", value);
 }
 void pop()
 {
-	if (isEmpty())
+	if(isEmpty())
 	{
 		printf("stack underflow! the stack is empt.\n");
 		return;
@@ -60,4 +60,35 @@ int main()
 	int choice, value;
 	printf("---stack operations using linked list ---\n");
 	while (1)
-
+	{
+	printf("\n1.push\n2.pop\n3.peek\n4.display\n5.exit\n");
+	printf("enter your choice:");
+	scanf("%d",&choice);
+	switch(choice)
+{
+	case 1:
+	printf("enter value to push:");
+	scanf("%d",&value);
+	push(value);
+	break;
+	case 2:
+	pop();
+	break;
+	case 3:
+	peek();
+	break;
+	case 4:
+	display();
+	break;
+	case 5:
+	printf("existing program....\n");
+	while(!isEmpty()){
+	pop();
+	}
+	exit(0);
+	default:
+	printf("invalid choice! please choose between 1 and 5.\n");
+}
+}
+return 0;
+}
